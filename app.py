909072e9de8e9690c8a034d2163ae6a80e7c93c9
pbullion <pbullion@gmail.com>
2017-01-05 12:57:53 -0600

    going live

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-"""
-Flask Documentation:     http://flask.pocoo.org/docs/
-Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
-Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
-
-This file creates your application.
-"""
+# """
+# Flask Documentation:     http://flask.pocoo.org/docs/
+# Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+# Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+#
+# This file creates your application.
+# """
 
 import os
 import pg
 from flask import Flask, render_template, request, redirect, url_for
-
+from tdmpfflClass import *
 import sys
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-app = Flask(__name__)
+app = Flask("MyApp")
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
@@ -28,48 +28,104 @@ DBNAME=os.environ.get('DBNAME', True)
 # Routing for your application.
 ###
 
-@app.route('/')
+@app.route("/")
 def home():
-    """Render website's home page."""
-    db=pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
-    query=db.query("select * from album")
-    result_list = query.namedresult()
-    return render_template('home.html',result_list=result_list)
+    return render_template("home.html")
 
-
-@app.route('/about/')
+@app.route("/about")
 def about():
-    """Render the website's about page."""
-    return render_template('about.html')
+    return render_template("about.html",title="HOW IT ALL BEGAN...") \
+
+@app.route("/champions")
+def champions():
+    return render_template("champions.html",title="PAST CHAMPIONS")\
+
+@app.route("/chickenbowl")
+def chickenbowl():
+    return render_template("chickenbowl.html",title="CHICKENS")
+
+@app.route("/standings")
+def standings():
+    return render_template("standings.html",title="Standings")
+
+@app.route("/members")
+def members():
+    result_list = Owners.getOwners()
+    return render_template("owners.html",result_list=result_list,title="Members of TDMPFFL")
+
+@app.route("/<idowners>/teamnames")
+def teamnames(idowners):
+    idowners = idowners
+    result_list = Teams.get_team_list(idowners)
+    return render_template("teamnames.html",team_names=result_list,title="All Teams for")
+
+@app.route("/alltime")
+def alltime():
+    result_list = Owners.alltime()
+    return render_template("alltime.html",result_list=result_list,title="All time stats")
+
+# @app.route("/<idowners>/owneryears")
+# def alltime(idowners):
+#     idowners = idowners
+#     result_list = Owners.owneryears(idowners)
+#     return render_template("owneryears.html",result_list=result_list,title="Owner by years")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+# @app.route('/')
+# def home():
+#     """Render website's home page."""
+#     db=pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
+#     query=db.query("select * from album")
+#     result_list = query.namedresult()
+#     return render_template('home.html',result_list=result_list)
+#
+#
+# @app.route('/about/')
+# def about():
+#     """Render the website's about page."""
+#     return render_template('about.html')
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
 
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# @app.route('/<file_name>.txt')
+# def send_text_file(file_name):
+#     """Send your static text file."""
+#     file_dot_text = file_name + '.txt'
+#     return app.send_static_file(file_dot_text)
+#
+#
+# @app.after_request
+# def add_header(response):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+#     response.headers['Cache-Control'] = 'public, max-age=600'
+#     return response
+#
+#
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     """Custom 404 page."""
+#     return render_template('404.html'), 404
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
---
+++ tdmpfflClass.py
@@ -0,0 +1,91 @@
+import json
+import pycurl
+import StringIO
+import urllib
+import mysql.connector
+import sys
+import pg
+import os
+
+DBUSER=os.environ.get('DBUSER', True)
+DBPASS=os.environ.get('DBPASS', True)
+DBHOST=os.environ.get('DBHOST', True)
+DBNAME=os.environ.get('DBNAME', True)
+
+db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
+
+# class Database(object):
+#     @staticmethod
+#     def getConnection():
+#         db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
+#         return
+#     @staticmethod
+#     def escape(value):
+#         return value.replace("'","''")
+#     @staticmethod
+#     def getResult(query,getOne=False):
+#
+#         Cursor.close()
+#         return result_set
+#     @staticmethod
+#     def doQuery(query):
+#         conn = Database.getConnection()
+#         cur = conn.cursor()
+#         cur.execute(query)
+#         conn.commit()
+#         lastId = cur.lastrowid
+#         cur.close()
+#         conn.close()
+#         return lastId
+
+class Owners(object):
+    def __init__(self,):
+        self.name = ""
+        self.yearstarted = 0
+        self.yearended = 0
+        self.idowners = 0
+    @staticmethod
+    def getOwners():
+        db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
+        query = db.query("SELECT * FROM owners order by yearstarted asc")
+        result_list = query.namedresult()
+        db.close()
+        return result_list
+    @staticmethod
+    def alltime():
+        db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
+        query = db.query("select owner,sum(wins) wins,sum(loss) loss,idowner from winloss group by owner,idowner order by sum(wins) desc")
+        result_list = query.namedresult()
+        db.close()
+        return result_list
+    # @staticmethod
+    # def owneryears(idowners):
+    #     db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
+    #     query = db.query("select 'year',sum(wins) wins,sum(loss) loss,idowners from winloss where idowners='%d' group by 'year',idowners order by year asc" % idowners)
+    #     result_list = query.namedresult()
+    #     db.close()
+    #     return result_list
+
+class Teams(object):
+    def __init__(self,idowners=0):
+        self.idteamnames=0
+        self.teamname = ""
+        self.yearstarted = 0
+        self.yearended = 0
+        self.idowners = idowners
+    @staticmethod
+    def get_team_list(idowners):
+        db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
+        query = db.query("SELECT * FROM teamnames where idowners='%s' order by yearstarted asc" % idowners)
+        result_list = query.namedresult()
+        db.close()
+        return result_list
+
+    # working on stats for each team
+    # @staticmethod
+    # def get_team_list(idowners):
+    #     db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
+    #     query = db.query("SELECT * FROM teamnames left join where idowners='%s' order by yearstarted asc" % idowners)
+    #     result_list = query.namedresult()
+    #     db.close()
+    #     return result_list
